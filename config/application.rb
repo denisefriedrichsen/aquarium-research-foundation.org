6b93e02c68035c46d53378d8d0933e5eec95c116
denise friedrichsen <denise.friedrichsen@yahoo.de>
2020-08-06 16:07:04 +0200

    made some changes to the about page

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require_relative 'boot'
 require 'rails/all'
 require 'devise'
 
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -17,6 +16,7 @@ module AquariumResearchFoundationOrg
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
